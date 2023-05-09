2805878115942f10b0557eb5046305847e7d4f28
Antonio <antonio_pereira_2008@hotmail.com>
2023-05-09 18:45:08 -0300

    teste

---
+++ assets/js/desafio.js
@@ -0,0 +1,22 @@
+let um = "A:10"
+let dois = "B: 20"
+let nulo = null
+let line = '<hr></hr>'
+
+
+document.write(um + '<br>');
+document.write(dois + '<br>');
+document.write('C: ' + nulo)
+document.write(line)
+
+if (true) {
+    let um = "A:20"
+    let dois = "B: 10"
+    let nulo = null
+    let line = '<hr></hr>'
+
+
+    document.write(um + '<br>');
+    document.write(dois + '<br>');
+    document.write('C: ' + nulo)
+}
