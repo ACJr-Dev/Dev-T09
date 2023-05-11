792a730e0d3b58c334f0a2aea69578e045b1e33a
Antonio <antonio_pereira_2008@hotmail.com>
2023-05-11 15:02:28 -0300

    desafio.js

--- assets/js/desafio.js
+++ assets/js/desafio.js
@@ -1,22 +1,16 @@
-// let um = 10
-// let dois = 20
-// let nulo = null
+let um = 10
+let dois = 20
+let nulo = null
 
 
-// document.write(`A: ${um} <br>`);
-// document.write(`B: ${dois} <br>`);
-// document.write(`C: ${nulo} <br>`);
-// document.write('<hr>');
+document.write(`A: ${um} <br>`);
+document.write(`B: ${dois} <br>`);
+document.write(`C: ${nulo} <br>`);
+document.write('<hr>');
 
-// um = 20
-// dois = 10
+um = 20
+dois = 10
 
-// document.write(`A: ${um} <br>`);
-// document.write(`B: ${dois} <br>`);
-// document.write(`C: ${nulo} <br>`);
-
-if (2 !== '2') {
-    document.write('entrou no if')
-} else {
-    document.write('entrou no else')
-}
+document.write(`A: ${um} <br>`);
+document.write(`B: ${dois} <br>`);
+document.write(`C: ${nulo} <br>`);
